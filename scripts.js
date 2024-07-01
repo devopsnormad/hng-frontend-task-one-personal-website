9cb379615d243bb83dbfb1fbf117572cf7252cc0
Emmanuel Doji <devopsnormad@gmail.com>
2024-07-01 20:31:23 +0100

    reformatting of code

--- scripts.js
+++ scripts.js
@@ -1,23 +1,31 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    // Function to update the current time in UTC
-    function updateTime(){
-        const now = new Date ();
-        const utcTime = now.toUTCString().split(' ')[4];
-        document.getElementById('current-time').textContent = utcTime;
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  // Function to update the current time in UTC
+  function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(" ")[4];
+    document.getElementById("current-time").textContent = utcTime;
+  }
 
-    //Function to update the current day of the week
-    function updateDay(){
-        const now = new Date ();
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const day = days[now.getUTCDay()];
-        document.getElementById('current-day').textContent = day;
-    }
+  //Function to update the current day of the week
+  function updateDay() {
+    const now = new Date();
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const day = days[now.getUTCDay()];
+    document.getElementById("current-day").textContent = day;
+  }
 
-    //Update time and day immediately
-    updateTime();
-    updateDay();
+  //Update time and day immediately
+  updateTime();
+  updateDay();
 
-    //Update time every second
-    setInterval(updateTime, 1000);
-})
+  //Update time every second
+  setInterval(updateTime, 1000);
+});
